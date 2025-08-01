91a223d492768d1c5f531a2e8e3d7d59439cc089
heaven443 <hcortez44359@liceo.edu.ph>
2025-08-01 11:09:47 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
+// Importing the Scanner to read the users input.
+import java.util.Scanner;
+
+public class numloop {
+
     public static void main(String[] args) {
-        System.out.println("Main Branch");
+        // Creating a Scanner object
+        Scanner numScan = new Scanner(System.in);
+            // then create a int object
+            int totNum = 0;
+
+        // Doing a for loop to repeatedly ask the user for numbers until the
+        // limit of 5 is reached
+        for (int i = 1; i <= 5; i++) {
+               // Every time the loop runs the "i" variable will be up by 1
+            System.out.print("Enter number " + i + ": ");
+            // every number that is entered will be added to the total sum of the number
+            int num = numScan.nextInt();
+            totNum += num;
+        }
+            // once the loop has finished it will print out the total number
+        System.out.println("Total Number: " + totNum);
+        numScan.close();
     }
 }
