4d3feb4714927a8ec56b791527a8e15766d00e98
heaven443 <hcortez44359@liceo.edu.ph>
2025-09-15 10:18:39 +0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,31 @@
+public class main {
+    public static void main(String[] args) {
 
+        petservice.main(args);
+
+        System.out.println("Welcome to the Pet Clinic");
+        System.out.println("=========================");
+        pet dog = new dog("Buddy", 3);
+        pet cat = new cat("Whiskers", 2);
+        pet bird = new bird("Tweety", 1);
+
+        dog.displayInfo();
+        dog.makeSound();
+
+        cat.displayInfo();
+        cat.makeSound();
+
+        bird.displayInfo();
+        bird.makeSound();
+
+        System.out.println("\nTraining Session Started");
+        System.out.println("=========================\n");
+
+        trainer trainer = new trainer();
+        trainableDog trainableDog = new trainableDog("Buddy", 3);
+        trainableBird trainableBird = new trainableBird("Tweety", 1);
+
+        trainer.trainPet(trainableDog);
+        trainer.trainPet(trainableBird);
+    }
+}
