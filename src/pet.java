8fcf08a09a2bf4937c115795f86fa7714020f7ed
heaven443 <hcortez44359@liceo.edu.ph>
2025-09-15 10:19:07 +0800

    Update pet.java

--- src/pet.java
+++ src/pet.java
@@ -1,1 +1,48 @@
+abstract class pet {
+    protected String name;
+    protected int age;
 
+    public pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public abstract void makeSound();
+
+    public void displayInfo() {
+        System.out.println("\nPet Name: " + name + ", Age: " + age);
+    }
+}
+
+    class dog extends pet {
+        public dog(String name, int age) {
+            super(name, age);
+        }
+
+        @Override
+        public void makeSound() {
+            System.out.println(name + " says: Woof! Woof!");
+        }
+    }
+
+    class cat extends pet {
+        public cat(String name, int age){
+            super(name, age);
+        }
+        @Override
+        public void makeSound() {
+                System.out.println(name + " says: Meow! Meow!");
+        }
+    }
+
+    class bird extends pet {
+        public bird(String name, int age){
+            super(name, age);
+        }
+        @Override
+        public void makeSound() {
+            System.out.println(name + " says: Tweet! Tweet!");
+        }
+
+    }
